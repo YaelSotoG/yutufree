4f28ce95828fa533582965a729e1330ff471959d
Yael Soto <xyarock@gmail.com>
2021-10-22 22:19:33 -0500

    se cambiara por youtube music en lo que descubro poner widgets en segundo plano

--- lib/src/models/youtube_model.dart
+++ lib/src/models/youtube_model.dart
@@ -1,10 +1,3 @@
-//AIzaSyDpRRdKmP51wwTUloTN7yauWkmJvsDoVg4
-//https://youtube.googleapis.com/youtube/v3/activities?part=snippet&key=[YOUR_API_KEY] //para actividades (sus me gusta, sus subs y su historial)
-//https://youtube.googleapis.com/youtube/v3/playlists?part=snippet&key=[YOUR_API_KEY]
-// To parse this JSON data, do
-//
-//     final welcome = welcomeFromJson(jsonString);
-
 import 'dart:convert';
 
 Welcome welcomeFromJson(String str) => Welcome.fromJson(json.decode(str));
